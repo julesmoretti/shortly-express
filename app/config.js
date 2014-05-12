3c40ed228d02cdd5f948271f493728654c2b9015
Hack Reactor Students <julesmoretti@me.com>
2014-05-12 15:58:44 -0700

    add schema for Users and Sessions

--- app/config.js
+++ app/config.js
@@ -45,5 +45,46 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.createTable('Users', function(table) {
+  table.increments('id');
+  table.string('username');
+  table.string('salt');
+  table.string('hash'); // salted hash
+});
+
+db.knex.schema.createTable('Sessions', function(table){
+  table.increments('id');
+  table.dateTime('loggedInAt');
+  table.dateTime('lastRequestAt');
+  // table.string('username');
+  table.integer('uid')
+    .references('id')
+    .inTable('Users');
+});
+
+// db.knex.schema.createTable('Lists', function(table){
+//   table.increments('id');
+//   table.varchar('uri');
+//   table.v
+// });
+// var Users = Bookshelf.Model.extend({
+//   tableName: 'users'
+// });
+
+// var customer = new User;
+
+// customer.set({first_name: "Joe", last_name: "Customer"});
+
+// return bookshelf.Knex.Schema.createTable("Sessions", function(table) {
+//   table.increments("id");
+//   table.dateTime("loggedInAt");
+//   table.dateTime("lastRequestAt");
+//   table.integer('userId')
+//     .unsigned()
+//     .references('id')
+//     .inTable('Users')
+//     .onDelete('CASCADE')  // optional
+//     .onUpdate(...) // optional
+// });
 
 module.exports = db;
